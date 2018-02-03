06615438841824b433a1e76b347de9caa6708429
Joseph KJ <josephkj20@gmail.com>
2018-02-03 19:57:50 +0530

    Trying to generate weights for rpn class labels

--- lib/nets/network.py
+++ lib/nets/network.py
@@ -102,6 +102,10 @@ class Network(object):
                                     [rpn_cls_prob, rpn_bbox_pred, self._im_info, self._mode,
                                      self._feat_stride, self._anchors, self._num_anchors],
                                     [tf.float32, tf.float32], name="proposal")
+
+      print('Going to print rois:')
+      print(rois)
+
       rois.set_shape([None, 5])
       rpn_scores.set_shape([None, 1])
 
