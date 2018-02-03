aa7b409e588bb87c991531ba26ce5f7120e53403
Joseph KJ <josephkj20@gmail.com>
2018-02-03 18:27:49 +0530

    Trying to generate weights for rpn class labels

--- lib/nets/network.py
+++ lib/nets/network.py
@@ -311,6 +311,7 @@ class Network(object):
       # print(tsrs)
       # print('---')
       print('get_tensor_by_name of the conv_5_3 activation:', tf.get_default_graph().get_tensor_by_name('vgg_16/conv5/conv5_3/weights:0'))
+      print('Value:', tf.get_default_graph().get_tensor_by_name('vgg_16/conv5/conv5_3/weights:0').eval())
       # with tf.variable_scope("pool5") as scope:
       rpn_labels = self._anchor_target_layer(rpn_cls_score, "anchor")
       # Try to have a deterministic order for the computing graph, for reproducibility
